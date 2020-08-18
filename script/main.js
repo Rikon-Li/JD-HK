570288bdc68080e4244f9626675258d18e52a272
ligen <ligen0018@outlook.com>
2020-08-19 00:42:00 +0800

    Dynamic data

--- script/main.js
+++ script/main.js
@@ -147,224 +147,129 @@ $('.item').hover(function () {
 
 
 console.log(111);
-$.ajax({
-  url: '../data/beauty.json',
-  type: 'post',
-  data: "",
-  dataType: 'json',
-  success: function (data) {
-    console.log(data);
-    var newDiv = `
-      <div class="beauty">
-        <div class="moduleList float-l">
-          <span>${data.listTitle}</span>
-          <ul></ul>
-        </div>
-        <a class="modulePic float-l" href="">
-          <img src="${data.modulePic}" alt="">
-          <div>
-            <h3>${data.modulePicTitle}</h3>
-            <p>查看更多&gt;</p>
+var mod = function(modNum){
+  $.ajax({
+    url: '../data/beauty.json',
+    type: 'post',
+    data: "",
+    dataType: 'json',
+    success: function (data) {
+      console.log(data[modNum]);
+      var newDiv = `
+        <div class="beauty">
+          <div class="moduleList float-l">
+            <span>${data[modNum].listTitle}</span>
+            <ul></ul>
           </div>
-        </a>
-        <div class="shopwindow float-l">
-          <div class="row first">
-            <a class="item big first" href="">
-              <div class="itemleft float-l">
-                <div class="itemleft-title">${data.block1['title']}</div>
-                <div class="price">
-                  <span class="item-act-price"><span class="rmb">¥</span>${data.block1['price']}</span>
-                  <span class="item-tag-price"><span class="rmb">¥</span>${data.block1['before']}</span>
-                </div>
-              </div>
-              <img src="${data.block1['pic']}" class="item-img">
-            </a>
-            <a class="item">
-              <img src="${data.block2['pic']}" class="item-img">
-              <div class="item-title">${data.block2['title']}</div>
-              <div class="price">
-                <span class="item-act-price"><span class="rmb">¥</span>${data.block2['price']}</span>
-                <span class="item-tag-price"><span class="rmb">¥</span>${data.block2['before']}</span>
-              </div>
-            </a>
-          </div>
-          <div class="row">
-
-            <a class="item">
-              <img src="${data.block3['pic']}" class="item-img">
-              <div class="item-title">${data.block3['title']}</div>
-              <div class="price">
-                <span class="item-act-price"><span class="rmb">¥</span>${data.block3['price']}</span>
-                <span class="item-tag-price"><span class="rmb">¥</span>${data.block3['before']}</span>
-              </div>
-            </a>
-    
-            <a class="item" href="">
-              <img src="${data.block4['pic']}" class="item-img">
-              <div class="item-title">${data.block4['title']}</div>
-              <div class="price">
-                <span class="item-act-price"><span class="rmb">¥</span>${data.block4['price']}</span>
-                <span class="item-tag-price"><span class="rmb">¥</span>${data.block4['before']}</span>
-              </div>
-            </a>
-    
-            <a class="item" href="">
-              <img src="${data.block5['pic']}" class="item-img">
-              <div class="item-title">${data.block5['title']}</div>
-              <div class="price">
-                <span class="item-act-price"><span class="rmb">¥</span>${data.block5['price']}</span>
-                <span class="item-tag-price"><span class="rmb">¥</span>${data.block5['before']}</span>
-              </div>
-            </a>
-            
-          </div>
-        </div>
-    
-    
-    
-        <div class="moduleSwiperWrap float-l">
-          <div class="title">
-            <span class="strong">BRAND</span>
-            ${data.sliderTitle}
-          </div>
-    
-          <div class="swiper-container moduleSwiper">
-            <div class="swiper-wrapper">
-              <div class="swiper-slide">
-                <div>
-                  <img src="${data.slider1[0][1]}" alt="">
-                  <span>${data.slider1[0][0]}</span>
-                </div>
-                <div>
-                  <img src="${data.slider1[1][1]}" alt="">
-                  <span>${data.slider1[1][0]}</span>
-                </div>
-                <div>
-                  <img src="${data.slider1[2][1]}" alt="">
-                  <span>${data.slider1[2][0]}</span>
-                </div>
-                <div>
-                  <img src="${data.slider1[3][1]}" alt="">
-                  <span>${data.slider1[3][0]}</span>
-                </div>
-                <div>
-                  <img src="${data.slider1[4][1]}" alt="">
-                  <span>${data.slider1[4][0]}</span>
-                </div>
-                <div>
-                  <img src="${data.slider1[5][1]}" alt="">
-                  <span>${data.slider1[5][0]}</span>
-                </div>
-                <div>
-                  <img src="${data.slider1[6][1]}" alt="">
-                  <span>${data.slider1[6][0]}</span>
-                </div>
-                <div>
-                  <img src="${data.slider1[7][1]}" alt="">
-                  <span>${data.slider1[7][0]}</span>
-                </div>
-              </div>
-              <div class="swiper-slide">
-                <div>
-                  <img src="${data.slider2[0][1]}" alt="">
-                  <span>${data.slider2[0][0]}</span>
-                </div>
-                <div>
-                  <img src="${data.slider2[1][1]}" alt="">
-                  <span>${data.slider2[1][0]}</span>
-                </div>
-                <div>
-                  <img src="${data.slider2[2][1]}" alt="">
-                  <span>${data.slider2[2][0]}</span>
-                </div>
-                <div>
-                  <img src="${data.slider2[3][1]}" alt="">
-                  <span>${data.slider2[3][0]}</span>
-                </div>
-                <div>
-                  <img src="${data.slider2[4][1]}" alt="">
-                  <span>${data.slider2[4][0]}</span>
-                </div>
-                <div>
-                  <img src="${data.slider2[5][1]}" alt="">
-                  <span>${data.slider2[5][0]}</span>
-                </div>
-                <div>
-                  <img src="${data.slider2[6][1]}" alt="">
-                  <span>${data.slider2[6][0]}</span>
-                </div>
-                <div>
-                  <img src="${data.slider2[7][1]}" alt="">
-                  <span>${data.slider2[7][0]}</span>
-                </div>
-              </div>
-              <div class="swiper-slide">
-                <div>
-                  <img src="${data.slider3[0][1]}" alt="">
-                  <span>${data.slider3[0][0]}</span>
-                </div>
-                <div>
-                  <img src="${data.slider3[1][1]}" alt="">
-                  <span>${data.slider3[1][0]}</span>
-                </div>
-                <div>
-                  <img src="${data.slider3[2][1]}" alt="">
-                  <span>${data.slider3[2][0]}</span>
-                </div>
-                <div>
-                  <img src="${data.slider3[3][1]}" alt="">
-                  <span>${data.slider3[3][0]}</span>
-                </div>
-                <div>
-                  <img src="${data.slider3[4][1]}" alt="">
-                  <span>${data.slider3[4][0]}</span>
-                </div>
-                <div>
-                  <img src="${data.slider3[5][1]}" alt="">
-                  <span>${data.slider3[5][0]}</span>
-                </div>
-                <div>
-                  <img src="${data.slider3[6][1]}" alt="">
-                  <span>${data.slider3[6][0]}</span>
-                </div>
-                <div>
-                  <img src="${data.slider3[7][1]}" alt="">
-                  <span>${data.slider3[7][0]}</span>
-                </div>
-              </div>
-              
+          <a class="modulePic float-l" href="">
+            <img src="${data[modNum].modulePic}" alt="">
+            <div>
+              <h3>${data[modNum].modulePicTitle}</h3>
+              <p>查看更多&gt;</p>
             </div>
-            <div class="swiper-pagination"></div>
+          </a>
+          <div class="shopwindow float-l">
+            <div class="row first">
+              <a class="item big first" href="">
+                <div class="itemleft float-l">
+                  <div class="itemleft-title">${data[modNum].block1['title']}</div>
+                  <div class="price">
+                    <span class="item-act-price"><span class="rmb">¥</span>${data[modNum].block1['price']}</span>
+                    <span class="item-tag-price"><span class="rmb">¥</span>${data[modNum].block1['before']}</span>
+                  </div>
+                </div>
+                <img src="${data[modNum].block1['pic']}" class="item-img">
+              </a>
+              <a class="item">
+                <img src="${data[modNum].block2['pic']}" class="item-img">
+                <div class="item-title">${data[modNum].block2['title']}</div>
+                <div class="price">
+                  <span class="item-act-price"><span class="rmb">¥</span>${data[modNum].block2['price']}</span>
+                  <span class="item-tag-price"><span class="rmb">¥</span>${data[modNum].block2['before']}</span>
+                </div>
+              </a>
+            </div>
+            <div class="row">
+              <a class="item">
+                <img src="${data[modNum].block3['pic']}" class="item-img">
+                <div class="item-title">${data[modNum].block3['title']}</div>
+                <div class="price">
+                  <span class="item-act-price"><span class="rmb">¥</span>${data[modNum].block3['price']}</span>
+                  <span class="item-tag-price"><span class="rmb">¥</span>${data[modNum].block3['before']}</span>
+                </div>
+              </a>
+              <a class="item" href="">
+                <img src="${data[modNum].block4['pic']}" class="item-img">
+                <div class="item-title">${data[modNum].block4['title']}</div>
+                <div class="price">
+                  <span class="item-act-price"><span class="rmb">¥</span>${data[modNum].block4['price']}</span>
+                  <span class="item-tag-price"><span class="rmb">¥</span>${data[modNum].block4['before']}</span>
+                </div>
+              </a>
+              <a class="item" href="">
+                <img src="${data[modNum].block5['pic']}" class="item-img">
+                <div class="item-title">${data[modNum].block5['title']}</div>
+                <div class="price">
+                  <span class="item-act-price"><span class="rmb">¥</span>${data[modNum].block5['price']}</span>
+                  <span class="item-tag-price"><span class="rmb">¥</span>${data[modNum].block5['before']}</span>
+                </div>
+              </a>
+            </div>
           </div>
-        </div>
-      </div>
-    `
-    $('.beautyWrap1').html(newDiv)
-    var lis = '';
-    $(data.list).each(function (index, el) {
-      lis += `<li>${el}</li>`;
-    })
-    console.log(lis);
-    $('.moduleList ul').html(lis);
-
-    var newBlock = '';
-    for (var i = 3; i <6; i++){
-
+          <div class="moduleSwiperWrap float-l">
+            <div class="title">
+              <span class="strong">BRAND</span>
+              ${data[modNum].sliderTitle}
+            </div>
+            <div class="swiper-container moduleSwiper">
+              <div class="swiper-wrapper">
+              </div>
+              <div class="swiper-pagination"></div>
+            </div>
+          </div>
+        </div>  
+      `
+      $(`.mod${modNum}`).html(newDiv);
+      var lis = '';
+      $(data[modNum].list).each(function (index, el) {
+        lis += `<li>${el}</li>`;
+      })
+      console.log(lis);
+      $('.moduleList ul').html(lis);
+  
+      // console.log(111);
+      $(data[modNum].slider).each(function (sliderNum, slider) {
+        // console.log(slider);
+        var newSlider = document.createElement('div');
+        $(newSlider).addClass(`swiper-slide slide${sliderNum}`);
+        // console.log(newSlider);
+        $(slider).each(function (imgNum,img){
+          $(newSlider).append(`
+            <div>
+              <img src="${img[1]}" alt="">
+              <span>${img[0]}</span>
+            </div>
+          `)
+        })
+        $('.swiper-wrapper').append(newSlider);
+        // console.log(document.querySelector('slider0'));
+      })
+  
+      var myModuleSwiper = new Swiper('.moduleSwiper', {
+        direction: 'horizontal', 
+        loop: true,
+        autoplay: {
+          delay: 3000, 
+        },
+        
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+        }
+      })
     }
+  })
+}
 
-
-    var myModuleSwiper = new Swiper('.moduleSwiper', {
-      direction: 'horizontal', 
-      loop: true,
-      autoplay: {
-        delay: 3000, 
-      },
-      
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      }
-    })
-  }
-})
-
+mod(0);
+mod(1);
